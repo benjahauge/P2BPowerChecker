23ff340c00fc2c905ac72dba58a3ecd4b00621cf
bhj <bhj@DESKTOP-C2ONRNM>
2023-02-06 14:59:11 +0100

    Add project files.

---
+++ P2BPowerChecker/P2BPowerChecker/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Android;
+using Android.Content.PM;
+using Plugin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace P2BPowerChecker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetBackground(Battery.ChargeLevel,
+            Battery.State == BatteryState.Charging);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Battery.BatteryInfoChanged -= Battery_BatteryInfoChanged;
+        }
+
+        private async void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            SetBackground(e.ChargeLevel, e.State == BatteryState.Charging);
+
+            if (e.State == BatteryState.Discharging)
+            {
+                string message = "Server har mistet strøm";
+                string number = "+4551328197";
+                var smsMessenger = CrossMessaging.Current.SmsMessenger;
+                if (smsMessenger.CanSendSmsInBackground)
+                {
+                    smsMessenger.SendSmsInBackground(number, message);
+                }
+            }
+        }
+
+        private void SetBackground(double level, bool charging)
+        {
+            Color? color = null;
+            var status = charging ? "Charging" : "Not charging";
+
+            if (level > .5f)
+            {
+                color = Color.Green.MultiplyAlpha(level);
+            }
+            else if (level > .1f)
+            {
+                color = Color.Yellow.MultiplyAlpha(1d - level);
+            }
+            else
+            {
+                color = Color.Red.MultiplyAlpha(1d - level);
+            }
+
+            BackgroundColor = color.Value;
+            LabelBatteryLevel.Text = status;
+            BatteryMessage.Text = "Current status is:";
+        }
+    }
+}
